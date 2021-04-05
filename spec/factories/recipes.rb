829c825dd6a44688424a326f94ae47ec91f1cf3a
lauretteroy <laurieroy.dev@gmail.com>
2021-04-04 21:28:20 -0600

    Add recipe index

--- spec/factories/recipes.rb
+++ spec/factories/recipes.rb
@@ -1,8 +1,14 @@
-# FactoryBot.define do
-# 	factory :recipe do
-# 		name { "Recipe 1"}
-# 		description { "Some Description"}
-# 		user { create(:user)} 
-# 		date  { Date.today }
-#   end
-# end
+FactoryBot.define do
+	factory :recipe do
+		name { "Recipe 1"}
+		description { "Some Description that's long enough"}
+		user { create(:user)} 
+	end
+
+	factory :second_recipe, class: Recipe do
+		name { "Recipe 2"}
+		description { "Some Description that's long enough"}
+		user { create(:user)} 
+	end
+	
+end
--- spec/features/recipe_spec.rb
+++ spec/features/recipe_spec.rb
@@ -1,22 +1,38 @@
 require 'rails_helper'
 
 describe 'navigate' do
-	# let(:user) { FactoryBot.create(:user)}
-	@user = User.create!(email: "tester@test.com", name: "tester", password: "password", password_confirmation: "password")
-	# let(:recipe) do
-	# @recipe =	Recipe.create(name: "Name1", description: "some crazy description for a recipe", user_id: @user.id)
-	@recipe =	@user.recipes.build(name: "Name1", description: "some crazy description for a recipe")
-	end
-
 	before do
-		login_as(@user)
+		# let(:user) { FactoryBot.create(:user)}
+		@user = User.create!(email: "tester@test.com", name: "tester", password: "password", password_confirmation: "password")
+		# let(:recipe) do
+		# @recipe =	Recipe.create(name: "Name1", description: "some crazy description for a recipe", user_id: @user.id)
+
+	# end
+
+	
+		# login_as(@user)
 	end
 
-	it 'can be reached successfully' do
-		expect(page.status_code).to eq(200)
-	end
+	describe 'index' do
+		before do
+			visit recipes_path 
+		end
 
-	it 'has a title of Recipes' do
-		expect(page).to have_content(/Recipes/)
+		it 'can be reached successfully' do
+			expect(page.status_code).to eq(200)
+		end
+
+		xit 'has a title of Recipes' do
+			expect(page).to have_content(/Recipes/)
+		end
+
+		end
+		it 'has a list of recipes' do
+			recipe1 =	FactoryBot.build_stubbed(:recipe)
+			recipe2 = FactoryBot.build_stubbed(:second_recipe)
+			
+			visit recipes_path 
+
+			expect(page).to have_content(/Recipe|1|2/)
 	end
 end
